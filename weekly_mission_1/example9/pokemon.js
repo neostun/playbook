49907fe0f8a5215bb6c495cabf236769cdda3201
neostun <neostun@gmail.com>
2022-04-15 03:01:19 -0500

    agregado pokemon.js

--- weekly_mission_1/example9/pokemon.js
+++ weekly_mission_1/example9/pokemon.js
@@ -1,10 +1,13 @@
-class pokemon {
-
+class Pokemon{
+  constructor(name) {
+    // this es una variable para referenciar el valor del contexto local de esta clase
+    this.name = name; // Estás variables se le conocen como atributos
+  }
+  sayHello() {
+    console.log(`Mi pokemon ${this.name} te saluda!!!`);
+  }
+  sayMessage(message) {
+    console.log(`Mi pokemon ${this.name} dice: ${message}`);
+  }
 }
-
-sayHello(){
-
-}
-sayMessage(){
-    
-}
+module.exports = Pokemon
