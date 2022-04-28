1b5773a56748ea900b3372054eb3e19e9a8b1603
neostun <neostun@gmail.com>
2022-04-28 03:33:14 -0500

    Ejemplos For Each

--- weekly_mission_2/examples1/examples1-2-forEach.js
+++ weekly_mission_2/examples1/examples1-2-forEach.js
@@ -3,4 +3,4 @@ let sum = 0; //la variable sum inicia en 0
 const numbers2 = [1,2,3,4,5]; //la constante tiene una lista de 5 numeros
 numbers2.forEach(num => sum += num) //aplica la operación de suma usando la lista numbers2 y lo agrega a la variable sum
 console.log("Ejemplo 2: Cálculo de la suma de los elementos de una lista")
-console.log(sum)
+console.log(sum)//
--- weekly_mission_2/examples1/examples1-3-forEach 2.js
+++ weekly_mission_2/examples1/examples1-3-forEach 2.js
@@ -1,4 +1,4 @@
 // Ejemplo 3: forEach para imprimir los países en letras mayúsculas
 const countries = ['Finland', 'Denmark', 'Sweden', 'Norway', 'Iceland']
 console.log("Ejemplo 5: Imprime la lista de países en mayúsculas")
-countries.forEach((element) => console.log(element.toUpperCase()))
+countries.forEach((element) => console.log(element.toUpperCase())) //
